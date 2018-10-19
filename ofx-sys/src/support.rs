de5a2840c4fce4ac2c24416d06937feb2ef8bef0
Nicola Orru <nigu.orru@gmail.com>
2018-10-20 00:14:23 +0100

    Found a bindgen configuration which compiles and passes tests (but it's probably unusable)

--- ofx-sys/src/support.rs
+++ ofx-sys/src/support.rs
@@ -2,4 +2,9 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+pub struct std_string {
+	_unused: [u8; 16]
+}
+pub struct std_basic_string {}
+
 include!(concat!(env!("OUT_DIR"), "/support_bindings.rs"));
