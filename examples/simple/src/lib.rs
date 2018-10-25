1809f5019fa1fa2279bbd1512779b9fecb935661
Nico Orru <nigu.orru@gmail.com>
2018-10-25 21:38:51 +0100

    POC compilation

--- examples/simple/src/lib.rs
+++ examples/simple/src/lib.rs
@@ -1,7 +1,46 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate ofx;
+
+use ofx::types::*;
+use ofx::*;
+use std::ffi::CStr;
+
+static mut HOST: Option<*mut OfxHost> = None;
+
+extern "C" fn set_host(host: *mut OfxHost) {
+	unsafe {
+		HOST = Some(host);
+	}
+}
+
+extern "C" fn entry_point(
+	action: CharPtr,
+	handle: VoidPtr,
+	in_args: OfxPropertySetHandle,
+	out_args: OfxPropertySetHandle,
+) -> Int {
+	0
+}
+
+static mut PLUGIN0: Option<OfxPlugin> = None;
+
+#[no_mangle]
+pub extern "C" fn OfxGetNumberOfPlugins() -> Int {
+	0
+}
+
+#[no_mangle]
+pub extern "C" fn OfxGetPlugin(nth: Int) -> *const OfxPlugin {
+	unsafe {
+		PLUGIN0 = Some(OfxPlugin {
+			pluginApi: CStr::from_bytes_with_nul_unchecked(b"\0").as_ptr(),
+			apiVersion: 0,
+			pluginVersionMajor: 0,
+			pluginVersionMinor: 0,
+			pluginIdentifier: CStr::from_bytes_with_nul_unchecked(b"\0").as_ptr(),
+			setHost: Some(set_host),
+			mainEntry: Some(entry_point),
+		});
+
+		(&PLUGIN0.unwrap()) as *const OfxPlugin
+	}
 }
--- ofx/src/lib.rs
+++ ofx/src/lib.rs
@@ -1,7 +1,28 @@
+#![allow(unused)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+extern crate ofx_sys;
+
+#[allow(unused)]
+pub mod types {
+	use std::os::raw::*;
+	pub type Int = c_int;
+	pub type UnsignedInt = c_uint;
+	pub type Char = c_char;
+	pub type CharPtr = *const c_char;
+	pub type CStr = *const c_char;
+	pub type Void = ();
+	pub type VoidPtr = *const c_void;
+}
+
+pub use ofx_sys::*;
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+	#[test]
+	fn it_works() {
+		assert_eq!(2 + 2, 4);
+	}
 }
