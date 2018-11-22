6d7df9a626d3efe8475da1567e43623d35bffc84
Nicola Orru <nigu.orru@gmail.com>
2018-11-22 22:17:11 +0000

    Auto enum names construction

--- ofx/src/enums.rs
+++ ofx/src/enums.rs
@@ -5,7 +5,9 @@ pub trait IdentifiedEnum: Sized {
 	fn from_bytes(ofx_name: &'static [u8]) -> Option<Self>;
 }
 
-macro_rules! identified_enum {{
+// TODO allow mixing
+macro_rules! identified_enum {
+	{
 		pub enum $name:ident {
 			$($key:ident => $value:ident),*
 		}
@@ -25,13 +27,35 @@ macro_rules! identified_enum {{
 			}
 
 			fn from_bytes(ofx_name: &'static [u8]) -> Option<Self> {
-				$(if ofx_name == $value {
-					Some($name::$key)
-				} else)
+				$(if ofx_name == $value { Some($name::$key) } else)
 				*
-				{
-					None
+				{ None }
+			}
+		}
+	};
+	{
+		pub enum $name:ident {
+			$($key:ident),*
+		}
+	} => {
+		#[derive(Copy, Clone)]
+		pub enum $name {
+			$($key),
+			*
+		}
+
+		impl IdentifiedEnum for $name {
+			fn to_bytes(&self) -> &'static [u8] {
+				match *self {
+					$($name::$key => concat_idents!(kOfx, $name, $key)),
+					*
 				}
+			}
+
+			fn from_bytes(ofx_name: &'static [u8]) -> Option<Self> {
+				$(if ofx_name == concat_idents!(kOfx, $name, $key) { Some($name::$key) } else)
+				*
+				{ None }
 			}
 		}
 	}
@@ -39,15 +63,24 @@ macro_rules! identified_enum {{
 
 identified_enum! {
 	pub enum ImageEffectContext {
-		Filter => kOfxImageEffectContextFilter,
-		General => kOfxImageEffectContextGeneral
+		Filter,
+		General
 	}
 }
 
 identified_enum! {
 	pub enum BitDepth {
-		Byte => kOfxBitDepthByte,
-		Short => kOfxBitDepthShort,
-		Float => kOfxBitDepthFloat
+		Byte,
+		Short,
+		Float
 	}
+}
+
+mod tests {
+	use super::*;
+	#[test]
+	fn auto_enum_names() {
+		assert!(ImageEffectContext::Filter.to_bytes() == kOfxImageEffectContextFilter);
+		assert!(ImageEffectContext::General.to_bytes() == kOfxImageEffectContextGeneral);
+	}	
 }
