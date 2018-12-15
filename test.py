15490b3054b610a00cc0c686ad95809ab6a30e8a
Nicola Orru <nigu.orru@gmail.com>
2018-12-15 18:12:04 +0000

    Loader test scripts

---
+++ test.py
@@ -0,0 +1,4 @@
+print("Loaded script")
+app1.createNode("net.itadinanta.ofx-rs.simple_plugin_1")
+quit()
+
--- test_in_natron.sh
+++ test_in_natron.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cargo build && cp target/debug/libsimple_plugin.so examples/simple/resources/simple_plugin.ofx.bundle/Contents/Linux-x86-64/simple_plugin.ofx && echo 'app.createNode("net.itadinanta.ofx-rs.simple_plugin_1")' | Natron -t
+cargo build && cp target/debug/libsimple_plugin.so examples/simple/resources/simple_plugin.ofx.bundle/Contents/Linux-x86-64/simple_plugin.ofx && Natron -t $PWD/test.py
