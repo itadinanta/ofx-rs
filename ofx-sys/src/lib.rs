f060729281d04672591d7a1aca364d03839d65ae
Nicola Orru <nigu.orru@gmail.com>
2018-12-18 22:52:59 +0000

    Acnkowledged common clippy lints from bindgen

--- ofx-sys/src/lib.rs
+++ ofx-sys/src/lib.rs
@@ -1,6 +1,9 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(clippy::const_static_lifetime)]
+#![allow(clippy::unreadable_literal)]
+
 pub mod support;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
