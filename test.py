a018a225115e5496dadcef58fcf529ef1e91c59f
Nico Orru <nigu.orru@gmail.com>
2018-12-18 14:26:40 +0000

    Test comp actually renders something

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 print("Test in progress...")
-app1.createNode("net.itadinanta.ofx-rs.simple_plugin_1")
+mask = app1.createNode("fr.inria.openfx.Checkerboard")
+source = app1.createNode("fr.inria.openfx.Checkerboard")
+under_test = app1.createNode("net.itadinanta.ofx-rs.simple_plugin_1")
+under_test.connectInput(0, source)
+under_test.connectInput(1, mask)
+
+write = app.createWriter("target/filtered_test_####.png")
+write.connectInput(0, under_test)
+
+app1.render(write, 1, 1, 1)
 quit()
 
