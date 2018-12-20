9a5480dc1b59c5430eef6b73853008a244f87e42
Nico Orru <nigu.orru@gmail.com>
2018-12-20 12:16:33 +0000

    Created ofx directory on the fly in test

--- test_in_natron.sh
+++ test_in_natron.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+mkdir -p examples/simple/resources/simple_plugin.ofx.bundle/Contents/Linux-x86-64/
 cargo build && cp target/debug/libsimple_plugin.so examples/simple/resources/simple_plugin.ofx.bundle/Contents/Linux-x86-64/simple_plugin.ofx && Natron -t $PWD/test.py
